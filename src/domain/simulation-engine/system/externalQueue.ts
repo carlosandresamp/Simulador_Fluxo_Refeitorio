e64be33fec1ff098f31fd6b555972f896fba1fc7
jyunior245 <francisniltodossantos@gmail.com>
2025-02-05 18:13:52 -0800

    Implementação da classe Fila Externa

--- src/domain/simulation-engine/system/externalQueue.ts
+++ src/domain/simulation-engine/system/externalQueue.ts
@@ -5,41 +5,27 @@ import { add } from "date-fns";
 import { Student } from "./student";
 
 //Classe de fila externa que recebe uma lista de alunos 
-export class ExternalLine{
-    public studentQuantity:Student[];
-    private listStudentsKey = "externalQueue"; 
+export class ExternalQueue{
+    public studentQuantity:Student[]; 
 
     constructor(){
-        this.studentQuantity = this.gettingAllFromLocalStorage();
+        this.studentQuantity = [];
     }
-
 
     toAddStudent(student:Student){
-        let addingStudentToTheLine = this.gettingAllFromLocalStorage();
-        addingStudentToTheLine.push(student);
-        this.savingLocalStorage(addingStudentToTheLine);
+        this.studentQuantity.push(student)
+        console.log("Novo aluno chegou a fila!");
     }
 
-    toRemoveAluno(student:Student){
-
-    }
-    
-    //Método auxiliar de armazenamento
-    savingLocalStorage(student:Array<Student>){
-        let listStudentsWaiting:string = JSON.stringify(student);
-        localStorage.setItem(this.listStudentsKey, listStudentsWaiting);
-    }
-
-    //Método auxiliar de busca por todos os dados armazenados.
-    gettingAllFromLocalStorage():Student[]{
-        let studentRegistered:string = localStorage.getItem(this.listStudentsKey);
-
-        if(studentRegistered != null){
-            return JSON.parse(studentRegistered);
-        }else{
-            return [];
-        }
+    toRemoveStudent():Student{
+        //Remove aluno pelo conceito de FIFO através de atriubuição do índice 
+        let studentIndex:Student = this.studentQuantity[0];
+        this.studentQuantity.splice(0, 1);
+        console.log("Aluno saiu da fila externa.");
+        return studentIndex;
     }
 }
+
+
 
 
