dfd97fe9dc15fc1e34fc1d9e2bf8b883a5102e39
jyunior245 <francisniltodossantos@gmail.com>
2025-02-05 23:28:35 -0800

    Implementação da classe fila interna

---
+++ src/domain/simulation-engine/system/internalQueue.ts
@@ -0,0 +1,32 @@
+import { ExternalQueue } from "./externalQueue";
+import { Student } from "./student";
+
+export class InternalQueue extends ExternalQueue{
+    maxCapacity:number;
+
+    constructor(maxCapacity:number, studentQuantity?:Student[]){
+        super(studentQuantity);
+        this.maxCapacity = maxCapacity;
+    }
+
+    toAddStudent(student: Student): void {
+        let countingStudents:number = 0;
+        let gettinStudents:Student[] = this.studentQuantity;
+
+        for(let i=0; i<gettinStudents.length; i++){
+            countingStudents++;
+            if(countingStudents > this.maxCapacity){
+                throw new Error("Fila interna cheia: espere esvaziar");
+            }
+        }
+
+        gettinStudents.push(student);
+        console.log("Aluno entrou na Fila Interna");
+    }
+
+    toRemoveStudent(): Student {
+        super.toRemoveStudent();
+        console.log("Fila interna")
+        return;
+    }
+}
