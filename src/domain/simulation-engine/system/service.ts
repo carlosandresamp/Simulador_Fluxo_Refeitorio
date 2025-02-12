43b7bc77af4f2f3dd550785298f492778f7c6530
Jyunior <francisniltodossantos@gmail.com>
2025-02-11 23:41:15 -0800

    Subindo diagrama, correções de encapsulamento

--- src/domain/simulation-engine/system/service.ts
+++ src/domain/simulation-engine/system/service.ts
@@ -1,8 +1,8 @@
 // Classe que representa o atendimento de um funcionário
-export class Service {
-    coWorkerRegister: string; // Identificador único do funcionário
-    coWorkerName: string; // Nome do funcionário responsável pelo atendimento
-    middleTimeService: number; // Tempo médio necessário para servir a comida
+export class Service{
+    private coWorkerRegister: string; // Identificador único do funcionário
+    private coWorkerName: string; // Nome do funcionário responsável pelo atendimento
+    private middleTimeService: number; // Tempo médio necessário para servir a comida
 
     /**
      * Construtor da classe Atendimento
