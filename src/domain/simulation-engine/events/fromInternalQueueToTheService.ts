81b3412a061c7a9b9baacb8ca6dcec09ceb1ab25
carlosandresamp <149840865+carlosandresamp@users.noreply.github.com>
2025-02-22 21:28:04 -0300

    Subindo eventos

--- src/domain/simulation-engine/events/fromInternalQueueToTheService.ts
+++ src/domain/simulation-engine/events/fromInternalQueueToTheService.ts
@@ -1,3 +1,9 @@
 export class FromInternalQueueToTheService{
-    
+    if (this.internalQueue.length > 0 && this.availableTables > 0) {
+        const student = this.internalQueue.shift();
+        console.log(`${student} está sendo atendido.`);
+        return student || null;
+    }
+    console.log("Nenhum aluno pode ser atendido agora.");
+    return null;
 }
--- src/domain/simulation-engine/events/fromServiceToTheTable.ts
+++ src/domain/simulation-engine/events/fromServiceToTheTable.ts
@@ -1,3 +1,9 @@
 export class FromServiceToTheTable{
-    
+    if (this.availableTables > 0) {
+        this.availableTables--;
+        console.log(`${student} foi para a mesa. Mesas disponíveis: ${this.availableTables}`);
+    } else {
+        console.log("Nenhuma mesa disponível no momento.");
+    }
 }
+
